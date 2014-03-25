6bd1adb2637125125bc335f89804d1718623f5b9
Igor Palaguta <igor.palaguta@gmail.com>
2014-03-25 17:17:04 +0200

    Add Reactive Cocoa engine

--- Example/IOSLinkedInAPI-Podexample/IOSLinkedInAPI-Podexample/IOSLinkedInAPI-Podexample/LIAViewController.m
+++ Example/IOSLinkedInAPI-Podexample/IOSLinkedInAPI-Podexample/IOSLinkedInAPI-Podexample/LIAViewController.m
@@ -8,62 +8,58 @@
 
 #import "LIAViewController.h"
 #import "AFHTTPRequestOperation.h"
-#import "LIALinkedInHttpClient.h"
 #import "LIALinkedInApplication.h"
+
+#import "LIAEngine.h"
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 static NSString* const LINKEDIN_CLIENT_ID = nil;//Your client id
 static NSString* const LINKEDIN_CLIENT_SECRET = nil;//Your client secret
 
 @interface LIAViewController ()
 
-@property (nonatomic, strong) LIALinkedInHttpClient* client;
+@property (nonatomic, strong) LIAEngine* engine;
 
 @end
 
 @implementation LIAViewController
 
-@synthesize client = _client;
+@synthesize engine = _engine;
 
-- (LIALinkedInHttpClient *)client {
-   if ( !_client )
+-(LIAEngine*)engine
+{
+   if ( !_engine )
    {
       NSParameterAssert(LINKEDIN_CLIENT_ID);
       NSParameterAssert(LINKEDIN_CLIENT_SECRET);
+
       LIALinkedInApplication *application = [LIALinkedInApplication applicationWithRedirectURL:@"http://www.ancientprogramming.com/liaexample"
                                                                                       clientId:LINKEDIN_CLIENT_ID
                                                                                   clientSecret:LINKEDIN_CLIENT_SECRET
                                                                                          state:@"DCEEFWF45453sdffef424"
                                                                                  grantedAccess:@[@"r_fullprofile", @"r_network"]];
-      _client = [LIALinkedInHttpClient clientForApplication:application presentingViewController:nil];
+      _engine = [ [ LIAEngine alloc ] initWithApplication: application presentingViewController: self ];
    }
-   return _client;
+   return _engine;
 }
 
 - (IBAction)didTapConnectWithLinkedIn:(id)sender {
-  [self.client getAuthorizationCode:^(NSString *code) {
-    [self.client getAccessToken:code success:^(NSDictionary *accessTokenData) {
-      NSString *accessToken = [accessTokenData objectForKey:@"access_token"];
-      [self requestMeWithToken:accessToken];
-    }                   failure:^(NSError *error) {
-      NSLog(@"Quering accessToken failed %@", error);
-    }];
-  }                      cancel:^{
-    NSLog(@"Authorization was cancelled by user");
-  }                     failure:^(NSError *error) {
-    NSLog(@"Authorization failed %@", error);
-  }];
-}
-
-- (void)requestMeWithToken:(NSString *)accessToken {
-   [self.client linkedInMethod: @"GET"
-                           URL: [NSURL URLWithString: [ NSString stringWithFormat:@"https://api.linkedin.com/v1/people/~?oauth2_access_token=%@&format=json", accessToken]]
-                       success: ^(NSDictionary* user)
+   [ [ [ self.engine accessToken ] flattenMap:
+      ^( NSString* access_token_ )
+      {
+         return [ self.engine signalWithMethod: @"GET"
+                                          path: [ NSString stringWithFormat: @"https://api.linkedin.com/v1/people/~?oauth2_access_token=%@&format=json", access_token_ ] ];
+      } ]
+    subscribeNext:
+    ^( id user_ )
     {
-       NSLog(@"current user %@", user);
+       NSLog(@"result: %@", user_);
     }
-                       failure: ^(NSError* error)
+    error:
+    ^( NSError* error_ )
     {
-       NSLog(@"error %@", error);
+       NSLog(@"error: %@", error_ );
     }];
 }
 
